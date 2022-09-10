5c850a3a38f7036d85a3a65a8fc53b59f183ea6b
songjuneki <dev.sjk0105@gmail.com>
2022-09-10 17:36:52 +0900

    Initial Commit

---
+++ Yoram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yoram
+//
+//  Created by 송준기 on 2022/09/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yoram/YoramApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoramApp.swift
+//  Yoram
+//
+//  Created by 송준기 on 2022/09/10.
+//
+
+import SwiftUI
+
+@main
+struct YoramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
